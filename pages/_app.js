d4b1aa1f99a5c2338dae974f84d26a84fa38eb4a
BE-CH <mikkel.l.bech@gmail.com>
2022-09-29 21:03:41 +0200

    + added plausable analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://plausible.io/js/plausible.js" data-domain="bechsolutions.dk" strategy="afterInteractive" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
