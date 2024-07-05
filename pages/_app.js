f7f8eeb3779b1f29047524a92f914c0b3f3dbaeb
BE-CH <mikkel.l.bech@gmail.com>
2024-07-05 21:17:14 +0200

    Delete all yarn

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,13 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://696a-5-103-62-50.ngrok.io/webinsights.js"
+        data-website-id="7753f961-ab88-4096-bfdc-6d06e6c9044f"
+        defer
+        async
+        strategy="afterInteractive"
+      />
       <Script src="https://plausible.io/js/plausible.js" data-domain="bechsolutions.dk" strategy="afterInteractive" />
       <Component {...pageProps} />
     </>
